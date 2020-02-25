1154669189501b6bc534afb4790e3719ec64c445
Eddie Cheng <local@example.test>
2020-02-25 14:47:39 +0800

    caculate in model successfully

---
+++ patients/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+class Patient(models.Model):
+
+  # Fields
+  name = models.CharField(max_length=10)
+  sbp_raw = models.IntegerField(default=0)
+  dbp_raw = models.IntegerField(default=0)
+  hr_raw = models.IntegerField(default=0)
+  sbp = models.IntegerField(default=0)
+  dbp = models.IntegerField(default=0)
+  hr = models.IntegerField(default=0)
+
+  # Methods
+  def __str__(self):
+    return self.name
+
+
+  # calculate for raw data
+  def save(self, *args, **kwargs):
+    super().save(*args, **kwargs)
+    self.dbp = self.dbp_raw /2
